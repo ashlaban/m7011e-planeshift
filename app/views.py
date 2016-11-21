31fc532593a5cfca6a1f1bf0aeae17e86e814fe4
mikhed-1 <mikhed-1@student.ltu.se>
2016-11-21 14:48:35 +0100

    Fix placeholder struct

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index.html')
 def index():
     return render_template('placeholder/description.html')
--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 
 from app import app
-app.run(debug=False, host='0.0.0.0', port=8081)
+app.run(debug=True, host='0.0.0.0', port=8081)
 
