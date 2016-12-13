c960c265a1113b31f272b3bb1a6378d880771cb1
ashlaban <ketost@gmail.com>
2016-12-13 18:21:21 +0100

    Fix paths for file upload - 2

--- config.py
+++ config.py
@@ -11,5 +11,5 @@ SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 # To disable warning about "significant overhead added by this feature".
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-UPLOAD_FOLDER        = './static/uploads'
+UPLOAD_FOLDER        = './app/static/uploads'
 STATIC_UPLOAD_FOLDER = '/static/uploads'
