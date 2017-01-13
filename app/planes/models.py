e7f6d366a5710f616be573590ffdeda263c33c3d
mikhed-1 <mikhed-1@student.ltu.se>
2017-01-13 13:19:01 +0100

    Add session table with api endpoints
    Warning: Endpoints not tested!

--- app/planes/models.py
+++ app/planes/models.py
@@ -46,6 +46,36 @@ class Plane(db.Model):
 	@staticmethod
 	def get_planes():
 		return Plane.query.all()
+
+	@staticmethod
+	def get_plane(name):
+		return Plane.query.filter_by(name=name).first()
 	
 	def __repr__(self):
 		return '<Name %r>' % (self.name)
+
+class Session(db.Model):
+	user = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+	plane = db.Column(db.Integer, db.ForeignKey('plane.id'), primary_key=True)
+
+	def get_user(self):
+		return User.query.get(int(self.user))
+
+	def get_plane(self):
+		return Plane.query.get(int(self.plane))
+
+	@staticmethod
+	def get_sessions():
+		return Session.query.all()
+
+	@staticmethod
+	def get_sessions(user):
+		return Session.query.filter_by(user=user.id).all()
+
+	@staticmethod
+	def get_sessions(plane):
+		return Session.query.filter_by(plane=plane.id).all()
+
+	@staticmethod
+	def get_session(user, plane):
+		return Session.query.filter_by(user=user.id, plane=plane.id).first()
