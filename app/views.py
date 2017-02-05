c49227c608499ef61481a17edccbc9ef13c835c7
ashlaban <ketost@gmail.com>
2017-02-05 14:13:05 +0100

    Fix: Forgot to relink unauth. index -> about

--- app/views.py
+++ app/views.py
@@ -25,11 +25,11 @@ def before_request():
 def index():
 	if g.user is not None and g.user.is_authenticated:
 		return redirect(url_for('profile_mod.show_profile_page'))
-	return render_template('index.html', title='Home')
+	return render_template('about.html')
 
 @app.route('/about')
 def about():
-	return render_template('about.html', title='About')
+	return render_template('about.html')
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
