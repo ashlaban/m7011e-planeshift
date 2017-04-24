cb5da779f4399f994dcc394b7f7832c52a89c1d6
ashlaban <ketost@gmail.com>
2017-04-24 11:00:01 +0200

    Add print to hook-server

--- external/hook-server.py
+++ external/hook-server.py
@@ -18,24 +18,37 @@ def redeploy_webserver(path, branch):
 	cwd = os.getcwd()
 	os.chdir(path)
 
+
+	print('Running command: ' + ' '.join(['sudo', 'systemctl', 'stop', 'planeshift-'+branch]))
 	subprocess.run(['sudo', 'systemctl', 'stop', 'planeshift-'+branch])
 	
+	print('Running command: ' + ' '.join(['git', 'stash']))
 	subprocess.run(['git', 'stash'])
+	print('Running command: ' + ' '.join(['git', 'pull', 'origin', branch]))
 	subprocess.run(['git', 'pull', 'origin', branch])
+	print('Running command: ' + ' '.join(['git', 'stash', 'pop']))
 	subprocess.run(['git', 'stash', 'pop'])
 
 	# The two lines below is replacement for the above, and handles local changes better
+	# print('Running command: ' + ' '.join(['git', 'fetch', 'origin', branch]))
 	# subprocess.run(['git', 'fetch', 'origin', branch])
+	# print('Running command: ' + ' '.join(['git', 'merge', '-s', 'recursive', '-X', 'theirs', 'origin/{}'.format(branch)]))
 	# subprocess.run(['git', 'merge', '-s', 'recursive', '-X', 'theirs', 'origin/{}'.format(branch)])
 	
+	print('Running command: ' + ' '.join(['git', 'checkout', branch]))
 	subprocess.run(['git', 'checkout', branch])
 
 	# Recreate the database and ensure permissions
+	print('Running command: ' + ' '.join(['rm', 'app.db']))
 	subprocess.run(['rm', 'app.db'])
+	print('Running command: ' + ' '.join(['./db_create.py'])))
 	subprocess.run(['./db_create.py']) 
+	print('Running command: ' + ' '.join(['./db_init.py']))
 	subprocess.run(['./db_init.py'])
+	print('Running command: ' + ' '.join(['chmod', 'g+w', 'app.db']))
 	subprocess.run(['chmod', 'g+w', 'app.db'])
 
+	print('Running command: ' + ' '.join(['sudo', 'systemctl', 'start', 'planeshift-'+branch]))
 	subprocess.run(['sudo', 'systemctl', 'start', 'planeshift-'+branch])
 
 	os.chdir(cwd)
